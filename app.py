4a0519c43326afe17861b4149fc6c3293b69d88f
Jaykumar Patel <jaykumarpatel@acies.holdings>
2024-09-13 10:48:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import asyncio
+import pandas as pd
+import pymongo
+myArts = pymongo.MongoClient("mongodb+srv://user:user@cluster0.u3fdtma.mongodb.net/ArtDB")
+mydb = myArts["ArtDB"]
+mycol= mydb["Art"]
+import requests
+from bs4 import BeautifulSoup
+
+def return_bio(x):
+    data = mycol.find_one({"ArtId":x})
+    url = data['URL']
+    r =  requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = soup.find('p')
+    bio = soup.get_text()
+    # bio = str(soup)
+    print(bio)
+    mycol.update_one({'ArtId' : x}, 
+                     {'$set' : {'details' : bio }})
+    
+
+# for x in range(1,50000):
+#     print(x)
+#     m = return_bio(x)
+#     print("-",m)
+    
+return_bio(50)
+
---
+++ app1.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import pymongo
+myArts = pymongo.MongoClient("mongodb+srv://user:user@cluster0.u3fdtma.mongodb.net/ArtDB")
+mydb = myArts["ArtDB"]
+mycol= mydb["Art"]
+
+def return_bio(x):
+    data = mycol.find_one({"ArtId": x})
+    url = data['URL']
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    bio = soup.find('p')
+    mycol.update_one({'ArtId': x}, {'$set': {'details': str(bio)}})
+    return x
+
+def main():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(return_bio, x) for x in range(1, 50000)]
+        for future in as_completed(futures):
+            x = future.result()
+            print(f"Processed ArtId: {x}")
+
+if __name__ == "__main__":
+    main()
